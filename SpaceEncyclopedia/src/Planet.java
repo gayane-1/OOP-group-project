3e54cf5477995274325dd747fe90719f57e0d7f7
Hrach <pap@Papis.Computer>
2024-04-04 01:07:29 +0400

    Hrach: Planets

---
+++ SpaceEncyclopedia/src/Planet.java
@@ -0,0 +1,20 @@
+public abstract class Planet
+{
+    private double surfaceTemperature;
+    private double orbitsSun;
+    private double spinsOnItsAxes;
+    private double axisTilt;
+
+    private double diameter;
+    private double averageDistanceFromSun;
+    private double orbitalSpeedAroundSun;
+    private double sunRiseTosunRise;
+    private double mass;
+    private double volume;
+    private double averageDensity;
+    private double surfaceGravity;
+    private double averageSurfaceTemperature;
+    private double numberOfMoons;
+
+
+}
